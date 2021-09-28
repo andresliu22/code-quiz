41b86827a3b0597af5bf1ca7a6a34ccb53797da4
Andres Liu <andres.liu22@gmail.com>
2021-09-28 08:38:39 -0700

    Add pseudocode

---
+++ Assets/js/script.js
@@ -0,0 +1,5 @@
+// Instantiate object with questions and answers
+// Add action on start quiz button to show first question and possible answers
+// Add a timer at the start of the quiz
+// Show a message that indicates if the answer was correct or wrong
+// At the end of the quiz show the highscores with options to go back or clear them
