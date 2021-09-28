6dfa4d021ce50cc33f9e73f07a7c4ba9d2029e78
Andres Liu <andres.liu22@gmail.com>
2021-09-28 11:04:08 -0700

    Add quiz

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,27 +8,98 @@
 var questions = [
     {
         question: "Commonly used data types DO NOT include:",
-        options: ["strings, booleans, alerts, numbers"],
+        options: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
     {
         question: "The condition in an if / else statement is enclosed within ______.",
-        options: ["quotes, curly brackets, parentheses, square brackets"],
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "curly brackets"
     },
     {
         question: "Arrays in Javascript can be used to store ______.",
-        options: ["numbers and strings, other arrays, booleans, all of the above"],
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
     },
     {
         question: "String values must be enclosed within ______ when being assigned to variables.",
-        options: ["commas, curly brackets, quotes, parentheses"],
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes"
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        options: ["Javascript, terminal / bash, for loops, console.log"],
+        options: ["Javascript", "terminal / bash", "for loops", "console.log"],
         answer: "console.log"
     }
 ]
+
+var main = document.querySelector('#main');
+var startForm = document.querySelector('#start-form');
+var startDiv = document.querySelector('#start-div');
+var optionList = document.querySelector('#option-list');
+var question = document.querySelector('#question');
+var questionDiv= document.querySelector('#question-div');
+
+var count = 0;
+var score = 0;
+var time = 30;
+
+function startQuiz(event) {
+    event.preventDefault()
+    console.log(count)
+    // Hid initial container
+    startDiv.setAttribute('style', 'display: none');
+
+    removeAllChilds(optionList)
+
+    question.textContent = questions[count].question;
+    
+
+    for (var i = 0; i < questions[count].options.length ; i++) {
+        var li = document.createElement('li');
+        li.className = "flex-row justify-space-between align-center p-2 bg-light text-dark";
+        li.setAttribute("style", 'list-style-type: none');
+        var option = document.createElement('option');
+        option.className = 'btn btn-primary submit-answer';
+        option.setAttribute("style", 'display: block; margin-bottom: 5px');
+        option.textContent = (i + 1) + '. '+ questions[count].options[i];
+        li.append(option);
+        optionList.append(li);
+
+        option.addEventListener("click", showNext);
+    }
+    
+}
+
+startForm.addEventListener("submit", startQuiz);
+
+function removeAllChilds(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function showNext(event) {
+    var answer = event.target.textContent;
+    if (answer.includes(questions[count].answer)) {
+        console.log("Correct");
+        score += 5;
+    } else {
+        console.log("Wrong");
+        time -= 5;
+    }
+    
+    count++;
+
+    if (questions.length > count) {
+        startQuiz(event);
+    } else {
+        showFinalScore()
+    }
+    
+};
+
+function showFinalScore() {
+    startDiv.css('display', 'none');
+    questionDiv.css('display', 'none');
+}
